13b36cb3793f616d3793a1e546199fa52853ed4c
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-29 14:42:40 -0600

    Add project files.

---
+++ Syntax Sugar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Syntax_Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            
+            //    response = answer + " is less than nine";
+            
+            //else
+            
+            //    response = answer + “greater than or equal to nine”;
+            
+
+
+
+
+            var answer = 4;
+
+            
+            Console.WriteLine($" {answer}  is <9");
+
+            
+            var response = (answer < 9) ? $"{answer}is less than nine" : $"{answer}is greater than nine";
+
+        }
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
